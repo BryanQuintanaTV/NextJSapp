6c4d323fc9ca209879feb7dfead2f55796fc47c4
BryanQuintanaTV <78245905+BryanQuintanaTV@users.noreply.github.com>
2022-11-09 10:53:51 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,24 +18,6 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
           <a
             href="holi"
             className="card"
